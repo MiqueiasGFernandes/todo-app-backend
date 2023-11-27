9d48aa251028f04a2bb20047df3be247ccbddb8e
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2023-11-26 23:25:31 -0300

    feat: add typeorm settings

--- src/domain/task/Task.module.ts
+++ src/domain/task/Task.module.ts
@@ -1,9 +1,17 @@
 import { Global, Module } from '@nestjs/common'
 import { AddTaskController } from './controllers/AddTask.controller'
 import { AddTaskService } from './services'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { TaskListModel, TaskModel } from '@domain/models'
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      TaskListModel,
+      TaskModel
+    ])
+  ],
   controllers: [AddTaskController],
   providers: [AddTaskService]
 })
