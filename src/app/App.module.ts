e583193847ece3301c758bfd6c1757b1ae493f2f
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2023-11-26 23:23:52 -0300

    feat: add typeorm configuration

--- src/app/App.module.ts
+++ src/app/App.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common'
-import { TaskModule } from 'src/domain/task/Task.module'
+import { TaskModule } from '@domain/task/Task.module'
+import { Global, Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import * as path from 'path'
 
+@Global()
 @Module({
-  imports: [TaskModule],
-  controllers: [],
-  providers: []
+  imports: [ConfigModule.forRoot({
+    envFilePath: [
+      path.join(__dirname, '/../../.env'),
+      path.join(__dirname, '/../../.env.' + process.env.NODE_ENV)
+    ]
+  }), TypeOrmModule.forRoot({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    type: 'postgres',
+    entities: [
+      path.join(__dirname, '/../domain/models/*.js'),
+      path.join(__dirname, '/../domain/models/*.ts')
+    ],
+    database: process.env.DB_NAME,
+    poolSize: 1,
+    logging: true,
+    synchronize: false
+  }), TaskModule]
 })
 export class AppModule {}
